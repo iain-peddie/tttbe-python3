d15787f3e2a680553b0c331c018f52ae55741d29
Iain Peddie <inr314159@hotmail.com>
2013-06-26 08:11:00 +0100

    Refactored to take advantage of setUp method having been created

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -4,16 +4,16 @@ from TestCase import TestCase
 from WasRun import WasRun
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
 
     def testSetUp(self):
-        test = WasRun("testMethod")
-        test.run()
-        assert(test.wasSetUp)
+        self.test.run()
+        assert(self.test.wasSetUp)
         
 if __name__ == "__main__":
     TestCaseTest("testRunning").run()
--- WasRun.py
+++ WasRun.py
@@ -4,12 +4,11 @@ from TestCase import TestCase
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
-        self.wasSetUp = None
         TestCase.__init__(self, name)
         self.name = name
         
     def setUp(self):
+        self.wasRun = None
         self.wasSetUp = 1
 
     def testMethod(self):
