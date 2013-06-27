5944f5d8e199e5a8c59b10b749e30501b3fd47a1
Iain Peddie <inr314159@hotmail.com>
2013-06-27 08:14:53 +0100

    Refactored testing to look at test log string rather than multiple flags

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -4,17 +4,11 @@ from TestCase import TestCase
 from WasRun import WasRun
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
 
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-
-    def testSetUp(self):
-        self.test.run()
-        assert("setUp" == self.test.log)
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod " == test.log)
         
 if __name__ == "__main__":
-    TestCaseTest("testRunning").run()
-    TestCaseTest("testSetUp").run()
+    TestCaseTest("testTemplateMethod").run()
--- WasRun.py
+++ WasRun.py
@@ -10,8 +10,9 @@ class WasRun(TestCase):
     def setUp(self):
         self.wasRun = None
         self.wasSetUp = 1
-        self.log = "setUp"
+        self.log = "setUp "
 
     def testMethod(self):
         self.wasRun = 1
+        self.log = self.log + "testMethod "
 
