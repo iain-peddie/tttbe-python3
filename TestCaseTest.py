0fe858c6a25cbad32692715da81a8dc26bcaa10c
Iain Peddie <inr314159@hotmail.com>
2013-06-27 19:43:41 +0100

    Exceptions in tests are handled by the runner

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -33,5 +33,5 @@ class TestCaseTest(TestCase):
 if __name__ == "__main__":
     TestCaseTest("testTemplateMethod").run()
     TestCaseTest("testResult").run()
-#    TestCaseTest("testFailedResult").run()
     TestCaseTest("testFailedResultFormatting").run()
+    TestCaseTest("testFailedResult").run()
