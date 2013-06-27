e168ce7b216541dc62f3b1740566f2f9f541d05e
Iain Peddie <inr314159@hotmail.com>
2013-06-27 19:28:34 +0100

    Updated test to try to count failing tests

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -14,7 +14,13 @@ class TestCaseTest(TestCase):
         test = WasRun("testMethod")
         result = test.run()
         assert("1 run, 0 failed" == result.summary())
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
         
 if __name__ == "__main__":
     TestCaseTest("testTemplateMethod").run()
     TestCaseTest("testResult").run()
+    TestCaseTest("testFailedResult").run()
